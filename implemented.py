709e7de198ba89d8e2978277bd714f2ec391be79
yraf2 <y.raf2018@yandex.ru>
2023-01-16 01:06:55 +0300

    18 домашка

--- implemented.py
+++ implemented.py
@@ -1,7 +1,32 @@
-# файл для создания DAO и сервисов чтобы импортировать их везде
+from dao.genre_dao import GenreDao
+from dao.movie_dao import MovieDao
+from dao.director_dao import DirectorDao
 
-# book_dao = BookDAO(db.session)
-# book_service = BookService(dao=book_dao)
-#
-# review_dao = ReviewDAO(db.session)
-# review_service = ReviewService(dao=review_dao)
+from service.genre_service import GenreService
+from service.movie_service import MovieService
+from service.director_service import DirectorService
+
+from dao.model.movie import MovieSchema
+from dao.model.genre import GenreSchema
+from dao.model.director import DirectorSchema
+
+from setup_db import db
+
+director_dao = DirectorDao(db.session)
+director_service = DirectorService(director_dao)
+
+director_schema = DirectorSchema()
+directors_schema = DirectorSchema(many=True)
+
+genre_dao = GenreDao(db.session)
+genre_service = GenreService(genre_dao)
+
+genre_schema = GenreSchema()
+genres_schema = GenreSchema(many=True)
+
+movie_dao = MovieDao(db.session)
+movie_service = MovieService(movie_dao)
+
+movie_schema = MovieSchema()
+movies_schema = MovieSchema(many=True)
+
